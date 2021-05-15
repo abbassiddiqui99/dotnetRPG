a6b4e6b8a7c8a9f38e95fc0cd4891e8906bf4608
Abbas Hussain <abbassiddiqui99@gmail.com>
2021-05-15 07:52:10 +0500

    jwt implemented

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,5 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Models;
 
 namespace Data
@@ -7,27 +14,30 @@ namespace Data
     public class AuthRepository : IAuthRepository
     {
         private readonly DataContext _context;
-        public AuthRepository(DataContext context)
+        private readonly IConfiguration _configuration;
+        public AuthRepository(DataContext context, IConfiguration configuration)
         {
+            _configuration = configuration;
             _context = context;
 
         }
         public async Task<ServiceResponse<string>> Login(string username, string password)
-        { 
+        {
             ServiceResponse<string> response = new ServiceResponse<string>();
             User user = await _context.Users.FirstOrDefaultAsync(x => x.Username.ToLower().Equals(username.ToLower()));
-            if(user == null)
+            if (user == null)
             {
                 response.Success = false;
                 response.Message = "User isn't exists";
             }
-            else if(!VerifyPasswordHash(password,user.PasswordHash,user.PasswordSalt))
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
             {
                 response.Success = false;
                 response.Message = "Wrong Password";
             }
-            else {
-                response.Data = user.Id.ToString();
+            else
+            {
+                response.Data = CreateToken(user);
             }
 
             return response;
@@ -37,7 +47,8 @@ namespace Data
         {
 
             ServiceResponse<int> response = new ServiceResponse<int>();
-            if (await UserExists(user.Username)){
+            if (await UserExists(user.Username))
+            {
                 response.Success = false;
                 response.Message = "User Already Exists";
                 return response;
@@ -54,21 +65,25 @@ namespace Data
 
         public async Task<bool> UserExists(string username)
         {
-            if (await _context.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower())){
+            if (await _context.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower()))
+            {
                 return true;
             }
             return false;
         }
 
-        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt){
-            using (var hmac = new System.Security.Cryptography.HMACSHA512()){
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
         }
 
-        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt){
-            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
             {
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
                 for (int i = 0; i < computedHash.Length; i++)
@@ -80,6 +95,33 @@ namespace Data
                 }
                 return true;
             }
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value)
+            );
+
+            SigningCredentials credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                 Subject = new ClaimsIdentity(claims),
+                 Expires = DateTime.Now.AddDays(1),
+                 SigningCredentials = credentials
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
